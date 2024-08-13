882bf65e45f1eabb01828c973921d643ce3fd07d
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-13 17:09:16 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,8 +1,11 @@
 #include <TM1637.h>
 #include <Wire.h>
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
-TM1637 display(13, 12);
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+
+TM1637 tmDisplay(13, 12);
 int trigPin = 23;
 int echoPin = 22;
 int distance;
@@ -15,7 +18,8 @@ unsigned long lastPressTime = 0;
 int startTime;
 int ledPin = 15;
 bool go;
-//Adafruit_SSD1306 display(128, 64, &Wire, -1);
+Adafruit_SSD1306 oledDisplay(128, 64, &Wire, -1);
+
 void setup() {
   Serial.begin(115200);
   pinMode(buttonPin, INPUT_PULLUP);
@@ -25,41 +29,56 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   lastState = digitalRead(buttonPin);
- 
   Serial.print("START");
+  oledDisplay.clearDisplay();
+  oledDisplay.setTextSize(1);
+  oledDisplay.setTextColor(SSD1306_WHITE);
+  oledDisplay.setCursor(0, 0);
+  tmDisplay.set(7);
+  tmDisplay.init();
+  oledDisplay.display();
+  oledDisplay.print("Push-up counter");
+
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH && lastState == LOW) {
+    oledDisplay.print("JUST DO IT");
     counter = 0;
     startTime = millis();
     lastState = buttonState; 
     go = true;
     Serial.print("pressed");
+    delay(10);
   }
   delay(10);
   if (go && millis() - startTime < 60000) {
+    
     checkDistance();
-    if (distance <= 15) {
-       
+    delay(10);
+  if (distance <= 15) {
       counter += 1;
       Serial.print("counter:");
       Serial.print(counter);
-      delay(100);
+      digitalWrite(ledPin, HIGH);
+      tone(buzzerPin, 256);
+      delay(10);
     }
-    display.display(3, counter);
     if (counter < 10) {
-      display.display(3, counter);
+      tmDisplay.display(3, counter);
     } else if (counter >= 10 && counter <= 99) {
-      display.display(2, counter / 10);
-      display.display(3, counter % 10);
+      tmDisplay.display(2, counter / 10);
+      tmDisplay.display(3, counter % 10);
     } else if (counter >= 100 && counter <= 999) {
-      display.display(1, counter / 100);
-      display.display(2, (counter / 10) % 10);
-      display.display(3, counter % 10);
+      tmDisplay.display(1, counter / 100);
+      tmDisplay.display(2, (counter / 10) % 10);
+      tmDisplay.display(3, counter % 10);
     }
     delay(10);
+    digitalWrite(ledPin, LOW);
+      noTone(buzzerPin);
+      delay(1000);
   } 
   else {
     delay(1000);
@@ -83,6 +102,5 @@ void checkDistance() {
 
 void clearDisplay() {
   int8_t clearData[4] = { 0x7F, 0x7F, 0x7F, 0x7F }; 
-  display.display(clearData);
+  tmDisplay.display(clearData);
 }
-
